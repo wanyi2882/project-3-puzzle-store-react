84f204cea5f3c0d096ccc14076959e2b5dcd60e7
wanyi2882 <wanyi.2882@gmail.com>
2021-12-02 09:36:52 +0000

    Cart update quanity

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -17,6 +17,32 @@ export default function Cart() {
         getCart()
     }, [])
 
+    const updateCartAdd = (index) => {
+
+        cart[index].quantity += 1;
+        const updateCart = [...cart];
+        setCart(updateCart);
+
+    }
+
+    const updateCartMinus = async (index, puzzle_id) => {
+
+        cart[index].quantity -= 1;
+        const updateCart = [...cart];
+        setCart(updateCart);
+
+        const quantity = cart[index].quantity
+
+        await axios.post(process.env.REACT_APP_URL + "/api/cart/quantity/update",
+        {
+            "puzzle_id": puzzle_id,
+            "newQuantity": quantity
+        }
+        , { headers: authorizationHeader() })
+
+    }
+
+
     // console.log(cart)
 
     return <React.Fragment>
@@ -29,8 +55,16 @@ export default function Cart() {
                             <img src={content.puzzle.image} alt={content.puzzle.title} width="200" />
                             <div className="card-body">
                                 <h6 className="card-title">{content.puzzle.title}</h6>
-                                <span>${(content.puzzle.cost / 100).toFixed(2)}</span>< br />
-                                <span>Quantity: {content.quantity}</span>
+                                <span>${(content.puzzle.cost / 100).toFixed(2)}</span>
+                                <div>Quantity:
+                                <button className="btn btn-primary btn-sm mx-1" 
+                                    onClick={() => updateCartMinus(cart.indexOf(content), content.puzzle.id)}>-</button>
+                                    <input type="number" value={content.quantity} style={{width: "50px"}}
+                                    />
+                                    <button className="btn btn-primary btn-sm mx-1" 
+                                    onClick={() => updateCartAdd(cart.indexOf(content), content.puzzle.id)}>+</button>
+                                </div>
+
                             </div>
                         </div>
                     </div>
