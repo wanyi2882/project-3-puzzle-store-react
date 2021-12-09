129cd02b8830d71d4f93d41c6242068c2bbe29c3
wanyi2882 <wanyi.2882@gmail.com>
2021-12-09 10:29:49 +0000

    Included landing page banner and implemented scroll

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { Link } from 'react-scroll'
+
+import { BsArrowDownCircle } from "react-icons/bs";
+
+import "../css/Landing.css"
+import ProductListing from './ProductListing';
 
 export default function Landing() {
+
     return (
         <React.Fragment>
-            <h1>Home Page</h1>
-        </React.Fragment>
+            <div id="landing-banner">
+                <div id="welcome-div">
+                    <h1>SHOP PUZZLES</h1>
+                </div>
+                <div id="down-arrow-to-searchbox">
+                <Link to="listings" spy={true} smooth={true}>
+                    <BsArrowDownCircle/>
+                </Link>                    
+                </div>
+            </div>
+
+            {/* On Click of Scoll Down Icon, page goes down to diplay the product listing */}
+            <div id="listings" className="container">
+                {ProductListing()}
+            </div>
+
+        </React.Fragment >
     )
 }
