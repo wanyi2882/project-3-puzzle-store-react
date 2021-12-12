1420c8d7a4adbb4c6be1982b8db9d0b16d5c719a
wanyi2882 <wanyi.2882@gmail.com>
2021-12-12 20:06:35 +0000

    Cart.js updated for non login users

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -9,12 +9,62 @@ export default function Cart() {
     const [cart, setCart] = useState([]);
     const [user, setUser] = useState("");
 
-    // Get Cart Contents
+    // Get Cart Contents if user is logged in
     const getCart = async () => {
-        const response = await axios.get(process.env.REACT_APP_URL + "/api/cart", { headers: authorizationHeader() })
-        const cartData = response.data
-        setCart(cartData)
-    };
+        if (localStorage.getItem("user")) {
+            const response = await axios.get(process.env.REACT_APP_URL + "/api/cart", { headers: authorizationHeader() })
+            const cartData = response.data
+
+            // Check if local storage has any cart
+            // If exist to combine with cartData
+            if (localStorage.getItem("cart")) {
+                // Get local cart from local storage
+                let localCart = JSON.parse(localStorage.getItem("cart"))
+
+                // 1. Merge both arrays
+                // 2. Use Reducer function
+                const clonedCartArray = Object.values([...cartData, ...localCart].reduce((previousValue, currentValue) => {
+                    // 3. Puzzle Id not the same
+                    if (!previousValue[currentValue.Puzzle.id]) {
+                        previousValue[currentValue.Puzzle.id] = currentValue;
+                    } else {
+                        // 4. Puzzle ID same so add up the quantity together
+                        previousValue[currentValue.Puzzle.id].quantity += currentValue.quantity;
+                    }
+                    return previousValue;
+                }, {}));
+                setCart(clonedCartArray)
+                localStorage.removeItem("cart")
+
+                clonedCartArray.map(async eachItem => {
+                    // If there is already more than 1 item in the cart, we update the cart
+                    if (eachItem.quantity > 1) {
+                        await axios.post(process.env.REACT_APP_URL + "/api/cart/quantity/update",
+                            {
+                                "puzzle_id": eachItem.Puzzle.id,
+                                "newQuantity": eachItem.quantity
+                            }
+                            , { headers: authorizationHeader() })
+                    } else if (eachItem.quantity == 1) {
+                        // If only one item in cart
+                        await axios.get(process.env.REACT_APP_URL + "/api/cart/add"
+                            + "?puzzle_id=" + eachItem.Puzzle.id,
+                            { headers: authorizationHeader() }
+                        )
+                    }
+                })
+            } else {
+                // If nothing in local storage cart
+                setCart(cartData)
+            }
+        } else if (localStorage.getItem("cart")) {
+            let localCart = JSON.parse(localStorage.getItem("cart"))
+            setCart(localCart)
+        } else {
+            // create an empty cart
+            localStorage.setItem("cart", JSON.stringify([]));
+        }
+    }
 
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem("user"))
@@ -103,14 +153,20 @@ export default function Cart() {
                 )}
             </div>
             <div>
-                {/* Quick Check to Stripe */}
+                {/* Checkout to Stripe */}
                 <form action={process.env.REACT_APP_URL + "/api/checkout/create-checkout-session"} method="POST">
                     <div style={{ visibility: 'hidden' }} >
                         <input name="userId" id="userId" value={user} />
                     </div>
-                    <button type="submit">
-                        Checkout
-                    </button>
+                    {localStorage.getItem("user") ?
+                        <button type="submit" className="btn btn-success">
+                            Checkout
+                        </button>
+                        :
+                        <button className="btn btn-danger">
+                            Login to checkout
+                        </button>
+                    }
                 </form>
             </div>
         </div>
